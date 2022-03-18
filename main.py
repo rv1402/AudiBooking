5400a33894630b3cff2eb59037fcd4fe6ebbc789
Rushil Venkateswar <rushilv14@gmail.com>
2022-03-18 09:36:50 +0530

    Add classes.

---
+++ main.py
@@ -0,0 +1,24 @@
+# imports
+
+# classes
+
+class Theater:
+
+class Seat:
+
+class Show:
+
+class ShowManager:
+
+class Salesperson:
+
+class Spectator:
+
+class AccountsClerk:
+
+class Transaction:
+
+class Ledger:
+
+class ManagementSystem:
+
