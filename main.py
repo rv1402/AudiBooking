699d472d47c6df5cf9b613941214b62f7da22c83
Rushil Venkateswar <rushilv14@gmail.com>
2022-03-25 16:31:01 +0530

    Restructure

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 # imports
+from datetime import datetime
 
 # classes
 
 class Theater:
+    def __init__(self, seatNum, seatType):
+        self.seatNum = seatNum
+        # Balcony and Ordinary seats
+        self.seatType = seatType
+        self.transactionID = None
+        self.allotmentStatus = False
+
+    def allot(self, ID):
+        self.allotmentStatus = True
+        self.transactionID = ID
+
+    def isAvailable(self):
+        return not self.allotmentStatus
+
+    def cancel(self):
+        self.allotmentStatus = False
+        self.transactionID = None
 
 class Seat:
 
